9aeab4a7eabd2089d58db1ba45a01e424c4e554e
michael2992 <michaelvanbreukelen509@gmail.com>
2023-11-26 20:46:21 +0100

    merging commits and adding comments

--- MILP.py
+++ MILP.py
@@ -11,35 +11,49 @@ import matplotlib.pyplot as plt
 # Cars need to have t0, d0 and v0 for each one
 
 
+
+
+
 #####################################
 ### Defining all initial settings ###
 #####################################
-
 # Subscription distance [m]
 d_subscription = 500
-# Number of connected vehicles
-n_CV = 0 # PLACEHOLDER
 # Length of the vehicle [m]
 L = 5
 # Intersection square width [m]
 W = 10
 # Response time of an autonomous vehicle [s]
 t_res = 0.5
+# Number of connected vehicles
+no_vehicles = 10
+# Safety gap (Headway h) [s] (parallel vehicles, includes safety factor)
+t_gap1 = 1  # seconds
+# Safety gap conflicting movements [s] aka delta t travel calculation  (perpendicular vehicles)
+t_gap2 = 7.5  # seconds
+# Maximum acceleration [m/s^2]
+a_max_acc = 3  # m/s^2
 # Maximum deceleration in emergency braking [m/s^2]
 a_max_dec = -4
-# Preferred average velocity or raod average speed [m/s]
+# Speed limit of the road  [m/s]
+v_max = 30  # m/s
+# Preferred average velocity or road average speed [m/s]
 v_avg = 15.63889
-# Speed limit of the road
-v_max = 0 # PLACEHOLDER
-# Maximum acceleration
-a_max_acc = 3
+# Calculated lower bound value for big M for constraint 3
+M_big = 2000  # for constraint 3
 
-# Safety gap (Headway h ) which includes safety factor [s] (for vehicles heading in the same direction)
-t_gap1 = 1
 
-# Safety gap conflicting movements and delta t travel calculation  (For vehicles crossing the intersection)
-t_gap2 , delta_t_travel = 7.5, 7.5
+# Car properties
+t0 = np.zeros(no_vehicles)
+v0 = np.ones(no_vehicles)*20
+d0 = np.ones(no_vehicles)*100
+t_desired = np.zeros(no_vehicles) # all initial times initialized to 0 for now
 
+random.seed(42)
+
+#################################
+### Defining MILP Model class ###
+#################################
 
 class MILP_Model:
     def __init__(self, name="milp", no_vehicles=10):
@@ -48,32 +62,7 @@ class MILP_Model:
 
 
 
-
-
-
-
-
-
-
-
-
-# Defining initial values
-no_vehicles = 10  # Number of vehicles
-t_gap1 = 1  # seconds
-t_gap2 = 7.5  # seconds
-a_max = 3  # m/s^2
-v_max = 30  # m/s
-M_big = 2000  # for constraint 3
-
-t0 = np.zeros(no_vehicles)
-v0 = np.ones(no_vehicles)*20
-d0 = np.ones(no_vehicles)*100
-
-
-random.seed(42)
 MILP = Model("milp")
-
-t_desired = np.zeros(no_vehicles) # all initial times initialized to 0 for now
 
 
 #######################################
@@ -110,9 +99,9 @@ MILP.update()
 C1 = {}
 # Constraint 1
 for k in range(no_vehicles):
-    v = np.sqrt(v0[k] ** 2 + 2 * a_max * d0[k])
-    dt1 = (min(v_max, v) - v0[k]) / a_max
-    dt2 = max(d0[k] - (v_max ** 2 - v0[k] ** 2) / (2 * a_max), 0) / v_max
+    v = np.sqrt(v0[k] ** 2 + 2 * a_max_acc * d0[k])
+    dt1 = (min(v_max, v) - v0[k]) / a_max_acc
+    dt2 = max(d0[k] - (v_max ** 2 - v0[k] ** 2) / (2 * a_max_acc), 0) / v_max
     t_min = t0[k] + dt1 + dt2
     C1[k] = MILP.addConstr(t[k] >= t_min, name="C2[%d]" % k)
 
@@ -146,16 +135,11 @@ MILP.update()
 ### Defining objective ###
 ##########################
 
-# Minimize cost function J = w_1 * J1 + w_2 * J2
-obj = LinExpr()
-w_1 = 0.5
-w_2 = 0.5
-
 # Variables and Constraints for objective functions
 t_slack = {}
 obj_constraints = {}
 
-# Adding J1 slack variable (+1 variables)
+# Adding J1 slack variable (+1 variable)
 t_slack["slackJ1"] = MILP.addVar(lb=0.0,
                                  vtype=GRB.CONTINUOUS,
                                  name="slack_delta_t_access")
@@ -164,6 +148,8 @@ t_slack["slackJ1"] = MILP.addVar(lb=0.0,
 for i in range(no_vehicles):
     obj_constraints[("constraintsJ1", i)] = MILP.addConstr(t_slack["slackJ1"] >= t[i],
                                                            name="cons_t_access[%d]"%i)
+
+MILP.update()
 
 # Adding J2 slack variables (+no_vehicles variables)
 for i in range(no_vehicles):
@@ -180,6 +166,7 @@ for i in range(no_vehicles):
                                                              name="cons_t_access_neg_difference[%d]"%i)
     j += 1
 
+MILP.update()
 
 # First term J1
 J1 = t_slack["slackJ1"]
@@ -189,13 +176,14 @@ J2 = 0
 for i in range(no_vehicles):
     J2 += t_slack[("slackJ2", i)]
 
+# Define objective function to be MINIMIZED with weights w_1 and w_2
+obj = LinExpr()
+w_1 = 0.5
+w_2 = 0.5
 obj += w_1 * J1 + w_2 * J2
-
-
 MILP.setObjective(obj, GRB.MINIMIZE)
 
-
+# Update and print model
 MILP.update()
 #MILP.write('MILP.lp')
-
 print(MILP)
