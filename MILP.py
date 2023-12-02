33a12721155ae8812ee2797142cb614042637d2a
michael2992 <michaelvanbreukelen509@gmail.com>
2023-12-02 14:55:26 +0100

    First working version of the model

--- MILP.py
+++ MILP.py
@@ -20,7 +20,7 @@ W = 10
 # Response time of an autonomous vehicle [s]
 t_res = 0.5
 # Number of connected vehicles
-no_vehicles = 10
+no_vehicles = 2
 # Safety gap (Headway h) [s] (parallel vehicles, includes safety factor)
 t_gap1 = 1  # seconds
 # Safety gap conflicting movements [s] aka delta t travel calculation  (perpendicular vehicles)
@@ -42,13 +42,23 @@ M_big = 2000  # for constraint 3
 #######################################
 random.seed(42)
 
-t0 = np.zeros(no_vehicles)  # t0 = d0/v0
-v0 = np.ones(no_vehicles)*20
-d0 = np.ones(no_vehicles)*100
+v0 = np.ones(no_vehicles) * v_avg
+d0 = np.ones(no_vehicles) * 100
+t0 = d0/v0  # t0 = d0/v0
+
+
+v0 = [20, 20]
+d0 = [1000, 1050]
+t0 = [50, 50.5]
+t_sim = 0
+
+
 
 K = {k: {'Direction': random.choice(['North', 'South', 'East', 'West'])}
      for k in list(np.arange(0, no_vehicles))}  # 0 = vertical 1 = horizontal
 
+K = {0: {"Direction": "South"},
+     1: {"Direction": "North"}}
 ##############################
 ### Defining Model classes ###
 ##############################
@@ -64,10 +74,11 @@ class Vehicle:
 
 
 class MILP_Model:
-    def __init__(self, name="milp", no_vehicles=10):
+    def __init__(self, name="milp", no_vehicles=10, t_sim=0):
         self.MILP = Model(name)
         self.no_vehicles = no_vehicles
         self.vehicles = [[Vehicle(h, K[h]["Direction"], d0[h], v0[h], t0[h])] for h in range(no_vehicles)]
+        self.t_sim = t_sim
 
     def initialize_variables(self):
         pass
@@ -97,13 +108,13 @@ B3 = {}  # Binary vars for constraint 3
 
 # Access times one per vehicle (+no_vehicles)
 for i in range(no_vehicles):
-    t[i] = MILP.addVar(lb=0.0, vtype=GRB.CONTINUOUS, name="t")
+    t[i] = MILP.addVar(lb=0.0, vtype=GRB.CONTINUOUS, name="t[%d]" % i)
 
 # Binary variable one per pair of vehicles (constr. 2 & 3) (+ no_vehicles*(no_vehicles+1))
 for i in range(no_vehicles):
-    for j in range(i+1, no_vehicles):
-        B2[i, j] = MILP.addVar(vtype=GRB.BINARY, name="B2[%d][%d]" % (i, j))
-        B3[i, j] = MILP.addVar(vtype=GRB.BINARY, name="B3[%d][%d]" % (i, j))
+    for j in range(i + 1, no_vehicles):
+        B2[i, j] = MILP.addVar(vtype=GRB.BINARY, name="B2[%d,%d]" % (i, j))
+        B3[i, j] = MILP.addVar(vtype=GRB.BINARY, name="B3[%d,%d]" % (i, j))
 
 MILP.update()
 
@@ -118,7 +129,7 @@ for k in range(no_vehicles):
     v = np.sqrt(v0[k] ** 2 + 2 * a_max_acc * d0[k])
     dt1 = (min(v_max, v) - v0[k]) / a_max_acc
     dt2 = max(d0[k] - (v_max ** 2 - v0[k] ** 2) / (2 * a_max_acc), 0) / v_max
-    t_min = t0[k] + dt1 + dt2
+    t_min = t_sim + dt1 + dt2
     C1[k] = MILP.addConstr(t[k] >= t_min, name="C2[%d]" % k)
 
 
@@ -130,19 +141,22 @@ for j in range(no_vehicles):
             pair = (j, k)
             C2[pair] = MILP.addConstr(t[j] - t[k] + M_big * B2[j, k] >= t_gap1, name="C2[%d,%d]" % (j, k))
             pair = (k, j)
-            C2[pair] = MILP.addConstr(t[k] - t[j] + M_big * B2[j, k] >= t_gap1, name="C2[%d,%d]" % (k, j))
+            C2[pair] = MILP.addConstr(t[k] - t[j] + M_big * (1 -B2[j, k]) >= t_gap1, name="C2[%d,%d]" % (k, j))
 
 MILP.update()
 
 # Constraint 3
 C3 = {}
+vert_dir = ["North", "South"]
+hor_dir = ["East", "West"]
 for j in range(no_vehicles):
     for k in range(j + 1, no_vehicles):
-        if K[j]["Direction"] != K[k]["Direction"]:
+        if (K[j]["Direction"] in vert_dir and K[k]["Direction"] in hor_dir) or (K[j]["Direction"] in hor_dir
+                                                                                and K[k]["Direction"] in vert_dir):
             pair = (j, k)
             C3[pair] = MILP.addConstr(t[j] - t[k] + M_big * B3[j, k] >= t_gap2, name="C3[%d,%d]" % (j, k))
             pair = (k, j)
-            C3[pair] = MILP.addConstr(t[k] - t[j] + M_big * B3[j, k] >= t_gap2, name="C3[%d,%d]" % (k, j))
+            C3[pair] = MILP.addConstr(t[k] - t[j] + M_big * (1 - B3[j, k]) >= t_gap2, name="C3[%d,%d]" % (k, j))
 
 MILP.update()
 
@@ -171,15 +185,15 @@ MILP.update()
 for i in range(no_vehicles):
     t_slack[("slackJ2", i)] = MILP.addVar(lb=0.0,
                                           vtype=GRB.CONTINUOUS,
-                                          name="slack_delta_t_access_abs[%d]"%i)
+                                          name="slack_delta_t_access_abs[%d]" % i)
 
 # Adding J2 Constraints (+2*no_vehicles constraints)
 for i in range(no_vehicles):
     j = 0
     obj_constraints[("constraintsJ2", i)] = MILP.addConstr(t_slack[("slackJ2", i)] >= (t[i] - t0[i]),
-                                                           name="cons_t_access_pos_difference[%d]"%i)
-    obj_constraints[("constraintsJ2", i+j)] = MILP.addConstr(t_slack[("slackJ2", i)] >= -(t[i] - t0[i]),
-                                                             name="cons_t_access_neg_difference[%d]"%i)
+                                                           name="cons_t_access_pos_difference[%d]" % i)
+    obj_constraints[("constraintsJ2", i + j)] = MILP.addConstr(t_slack[("slackJ2", i)] >= -(t[i] - t0[i]),
+                                                               name="cons_t_access_neg_difference[%d]" % i)
     j += 1
 
 MILP.update()
@@ -187,22 +201,32 @@ MILP.update()
 # First term J1
 J1 = t_slack["slackJ1"]
 
-#Second term J2
+# Second term J2
 J2 = 0
 for i in range(no_vehicles):
     J2 += t_slack[("slackJ2", i)]
 
 # Define objective function to be MINIMIZED with weights w_1 and w_2
 obj = LinExpr()
-w_1 = 0.5
-w_2 = 0.5
+w_1 = 0
+w_2 = 1
 obj += w_1 * J1 + w_2 * J2
 MILP.setObjective(obj, GRB.MINIMIZE)
 
 # Update and print model
 MILP.update()
-#MILP.write('MILP.lp')
+# MILP.write('MILP.lp')
+
+MILP.write('MILP.lp')
+MILP.optimize()
+
+
+solution = []
+for i in MILP.getVars():
+    solution.append([i.VarName, i.X])
+    print(f"{i.VarName} = {i.X}")
 print(MILP)
+
 
 
 
